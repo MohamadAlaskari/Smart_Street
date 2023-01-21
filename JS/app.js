87fc29409cd798b3ce29a136e917b50b53d6da0d
MohamadAlaskari <m_alskari96@hotmail.com>
2023-01-21 14:39:50 +0100

    animation fix

--- JS/app.js
+++ JS/app.js
@@ -1,26 +1,9 @@
 //variabels
+const myElements = document.querySelectorAll(".xl");
+const myElements2 = document.querySelectorAll(".y");
+const myElements3 = document.querySelectorAll(".xr");
 
 //variabels für animation
-const home_headline = document.getElementById('home-headline');
-const home_subtext = document.getElementById('home-subtext');
-//about
-const about_headline = document.getElementById('about-headline');
-const about_subtext = document.getElementById('about-subtext');
-const about_text = document.getElementById('about-text');
-//vorteile
-const vorteile_headline = document.getElementById('vorteile-headline');
-const vorteile_subtext = document.getElementById('vorteile-subtext');
-const vorteilebox1 = document.getElementById('vorteilebox1');
-const vorteilebox2 = document.getElementById('vorteilebox2');
-const vorteilebox3 = document.getElementById('vorteilebox3');
-const vorteilebox4 = document.getElementById('vorteilebox4');
-const vorteilebox5 = document.getElementById('vorteilebox5');
-const vorteilebox6 = document.getElementById('vorteilebox6');
-//team
-const team_headline = document.getElementById('team-headline');
-const team_subtext = document.getElementById('team-subtext');
-
-
 
 //ende variabels für animation
 
@@ -111,34 +94,76 @@ function show_button() {
     this.scrollY > 205 && this.scrollY < 690 ? square.style.cssText = "transition: all 1s ease-out; opacity: 0.7; " : square.style.cssText = "transition: all 1s ease-out;opacity: 0";
     //hidden square an y > 140
     this.scrollY > 140 ? einstellungen.classList.add("hidden") : einstellungen.classList.remove("hidden");
-    //animation
-    this.scrollY < 100 ? home_headline.classList.add("slide-in-x") : home_headline.classList.remove("slide-in-x");
-    this.scrollY < 100 ? home_subtext.classList.add("slide-in-y") : home_subtext.classList.remove("slide-in-y");
-    //about
-    this.scrollY >= 160 ? about_headline.classList.add("slide-in-x") : about_headline.classList.remove("slide-in-x");
-    this.scrollY >= 161 ? about_subtext.classList.add("slide-in-y") : about_subtext.classList.remove("slide-in-y");
-    this.scrollY >= 163 ? about_text.classList.add("slide-in-y-button") : about_text.classList.remove("slide-in-y-button");
-    //vorteile
-
-    this.scrollY >= 800 ? vorteile_headline.classList.add("slide-in-x") : vorteile_headline.classList.remove("slide-in-x");
-    this.scrollY >= 800 ? vorteile_subtext.classList.add("slide-in-y") : vorteile_subtext.classList.remove("slide-in-y");
-    this.scrollY >= 810 ? vorteilebox1.classList.add("slide-in-x-left") : vorteilebox1.classList.remove("slide-in-x-left");
-    this.scrollY >= 810 ? vorteilebox2.classList.add("slide-in-x-right") : vorteilebox2.classList.remove("slide-in-x-right");
-    this.scrollY >= 950 ? vorteilebox3.classList.add("slide-in-x-left") : vorteilebox3.classList.remove("slide-in-x-left");
-    this.scrollY >= 950 ? vorteilebox4.classList.add("slide-in-x-right") : vorteilebox4.classList.remove("slide-in-x-right");
-    this.scrollY >= 1320 ? vorteilebox5.classList.add("slide-in-y-button") : vorteilebox5.classList.remove("slide-in-y-button");
-    this.scrollY >= 1320 ? vorteilebox6.classList.add("slide-in-x-right") : vorteilebox6.classList.remove("slide-in-x-right");
-    //team
-
-    this.scrollY >= 2000 ? team_headline.classList.add("slide-in-x") : team_headline.classList.remove("slide-in-x");
-    this.scrollY >= 2000 ? team_subtext.classList.add("slide-in-y") : team_subtext.classList.remove("slide-in-y");
-    this.scrollY >= 2070 ? teambox1.classList.add("slide-in-x-left") : teambox1.classList.remove("slide-in-x-left");
-    this.scrollY >= 2073 ? teambox2.classList.add("slide-in-y-button") : teambox2.classList.remove("slide-in-y-button");
-    this.scrollY >= 2075 ? teambox3.classList.add("slide-in-x-right") : teambox3.classList.remove("slide-in-x-right");
-
 
   }
 }
+
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.intersectionRatio > 0) {
+      // Element is now visible in the viewport
+      if (entry.target.classList.contains("xl")) {
+        entry.target.classList.add("slide-in-xl");
+      }  if (entry.target.classList.contains("y")) {
+        entry.target.classList.add("slide-in-y");
+      } if (entry.target.classList.contains("xr")) {
+        entry.target.classList.add("slide-in-xr");
+      }
+      /*
+       if (entry.target.classList.contains("yb")) {
+        entry.target.classList.add("slide-in-y-button");
+      }
+      if (entry.target.classList.contains("xl")) {
+        entry.target.classList.add("slide-in-x-left");
+      }
+      if (entry.target.classList.contains("xr")) {
+        entry.target.classList.add("slide-in-x-right");
+      }*/
+    } else {
+      // Element is no longer visible in the viewport
+      if (entry.target.classList.contains("xl")) {
+        entry.target.classList.remove("slide-in-xl");
+      }  if (entry.target.classList.contains("y")) {
+        entry.target.classList.remove("slide-in-y");
+      }if (entry.target.classList.contains("xr")) {
+        entry.target.classList.remove("slide-in-xr");
+      } 
+      /*
+       if (entry.target.classList.contains("yb")) {
+        entry.target.classList.remove("slide-in-y-button");
+      }
+       if (entry.target.classList.contains("xl")) {
+        entry.target.classList.remove("slide-in-x-left");
+      }
+      if (entry.target.classList.contains("xr")) {
+        entry.target.classList.remove("slide-in-x-right");
+      }*/
+    }
+  });
+});
+
+
+myElements.forEach(myElement => observer.observe(myElement));
+myElements2.forEach(myElement => observer.observe(myElement));
+myElements3.forEach(myElement => observer.observe(myElement));
+
+
+/*
+const myElements3 = document.querySelectorAll(".yb");
+myElements3.forEach(myElement => observer.observe(myElement));
+const myElements4 = document.querySelectorAll(".xl");
+myElements4.forEach(myElement => observer.observe(myElement));
+const myElements5 = document.querySelectorAll(".xr");
+myElements5.forEach(myElement => observer.observe(myElement));
+
+*/
+
+
+
+
+
+
 
 //scroll To Top beim clicken auf dem button
 function scrollToTop() {
@@ -154,7 +179,6 @@ function scrollToTop() {
 
 
 
-
 function app() {
   console.log("I'm from function app :), the start functio of JS");
   show_button();
@@ -162,7 +186,7 @@ function app() {
   set_darktheme();
   set_purbel_theme();
   show_einstellungen();
-
+  animation();
 
 }
 app();
